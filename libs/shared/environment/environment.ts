dde2b25453cb0c25656fe6965efa2aab5f5b71bb
Ricardo Espinoza <ricardo.evz19@gmail.com>
2025-11-06 20:26:53 -0600

    fix(environment): update API_URL to production endpoint and comment out local URL

--- libs/shared/environment/environment.ts
+++ libs/shared/environment/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
     '728116258989-oe719k2gf7ec5h0tob0v285bkertf0gt.apps.googleusercontent.com',
   WEB_GOOGLE_CLIENT_ID:
     '728116258989-p0r0ui0tg472scga7gb1o4nsfg7ejut1.apps.googleusercontent.com',
-  // API_URL: 'https://bb-api-y0vm.onrender.com/api/v1',
-  API_URL: 'http://localhost:3000/api/v1',
+  API_URL: 'https://bb-api-y0vm.onrender.com/api/v1',
+  // API_URL: 'http://localhost:3000/api/v1',
   STRIPE_PUBLISHABLE_KEY:
     'pk_test_51RYEfqPJwPYM8uTUSzJynUl2wpW3QZu5MshEneuaIwPnkPKz0L1tD6jISeB1ICgGS5zW7UGbVZ8ssJKXSuVM2IYK00SBjAS5mS',
   MAX_SIZE_FILE_AWS_S3: 10 * 1024 * 1024 * 1024,
