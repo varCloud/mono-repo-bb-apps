bd08d5e6abb8f4589d0ded3a9c3c4a3f249083b0
Ricardo Espinoza <ricardo.evz19@gmail.com>
2025-11-27 11:47:32 -0600

    feat(detail-workout): add video player for workout details

--- apps/athlete/src/app/features/workouts/pages/detail-workout/detail-workout.ts
+++ apps/athlete/src/app/features/workouts/pages/detail-workout/detail-workout.ts
@@ -1,7 +1,9 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   signal,
+  ViewChild,
   type OnInit,
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -19,6 +21,7 @@ import { LayoutContentComponent } from '@monorepo-bb-app/ui';
 import { TranslateModule } from '@ngx-translate/core';
 import { addIcons } from 'ionicons';
 import { heart, heartOutline } from 'ionicons/icons';
+import * as PlyrModule from 'plyr';
 
 @Component({
   selector: 'app-detail-workout',
@@ -39,14 +42,45 @@ import { heart, heartOutline } from 'ionicons/icons';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DetailWorkout implements OnInit {
+  @ViewChild('player') videoElement: ElementRef<HTMLVideoElement>;
   isFavorite = signal<boolean>(false);
   data = this.activatedRoute.snapshot.data['workout'];
+  player: Plyr;
+  videoUrl = this.data.assets[0].signedUrl || null;
   constructor(private activatedRoute: ActivatedRoute) {
     addIcons({ heart, heartOutline });
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.videoUrl, this.data);
+  }
 
   toggleFavorite($event: Event) {
     this.isFavorite.set(!this.isFavorite());
+  }
+
+  ngAfterViewInit() {
+    this.player = new PlyrModule.default(this.videoElement.nativeElement, {
+      ratio: '1:1',
+      hideControls: true,
+      clickToPlay: true,
+      playsinline: true,
+      controls: [
+        'play-large', // The large play button in the center
+        // 'restart', // Restart playback
+        'rewind', // Rewind by the seek time (default 10 seconds)
+        'play', // Play/pause playback
+        'fast-forward', // Fast forward by the seek time (default 10 seconds)
+        'progress', // The progress bar and scrubber for playback and buffering
+        'current-time', // The current time of playback
+        'duration', // The full duration of the media
+        'mute', // Toggle mute
+        // 'volume', // Volume control
+        // 'captions', // Toggle captions
+        'fullscreen', // Toggle fullscreen
+        'settings', // Settings menu
+        // 'pip', // Picture-in-picture (currently Safari only)
+        // 'airplay', // Airplay (currently Safari only)
+      ],
+    } as any);
   }
 }
