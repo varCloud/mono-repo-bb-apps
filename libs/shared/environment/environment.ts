d1df50a0ec52df8483234f903effd52bc63653d1
sapitopicador <sapitopicador@gmail.com>
2026-04-23 17:16:08 -0600

    feat: update API URL in environment and enhance password input with toggle in reset password form

--- libs/shared/environment/environment.ts
+++ libs/shared/environment/environment.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: false,
   IOS_GOOGLE_CLIENT_ID: '728116258989-oe719k2gf7ec5h0tob0v285bkertf0gt.apps.googleusercontent.com',
   WEB_GOOGLE_CLIENT_ID: '728116258989-p0r0ui0tg472scga7gb1o4nsfg7ejut1.apps.googleusercontent.com',
-  API_URL: 'https://bb-api-y0vm.onrender.com/api/v1',
+  API_URL: 'https://api.bodybooster.com.mx/api/v1',
+  //API_URL: 'https://bb-api-y0vm.onrender.com/api/v1',
   // API_URL: 'http://localhost:3000/api/v1',
   STRIPE_PUBLISHABLE_KEY:
     'pk_test_51RYEfqPJwPYM8uTUSzJynUl2wpW3QZu5MshEneuaIwPnkPKz0L1tD6jISeB1ICgGS5zW7UGbVZ8ssJKXSuVM2IYK00SBjAS5mS',
